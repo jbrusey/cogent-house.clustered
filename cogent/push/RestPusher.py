a9121c71dd7fbadc9520f9d48c238c1cc1134547
Dan Goldsmith <djgoldsmith@googlemail.com>
2014-01-06 16:58:03 +0000

    More Fixes to restpusher to new sync_nodes function is actually called

--- cogent/push/RestPusher.py
+++ cogent/push/RestPusher.py
@@ -440,7 +440,7 @@ class Pusher(object):
         self.save_mappings()
 
         #Moved here for the Sampson Version
-        #self.sync_nodes()
+        self.sync_nodes()
 
         session = self.localsession()
         houses = session.query(self.House)
@@ -451,7 +451,7 @@ class Pusher(object):
             #Look for the Last update
             lastUpdate = self.get_lastupdate(item)
             log.debug("Last Update was {0}".format(lastUpdate))
-            self.sync_nodeLocations(item) #Update node locations
+            #self.sync_nodeLocations(item) #Update node locations
             out = self.upload_readings(item, lastUpdate)
             log.debug("Upload Readings Returns {0}".format(out))
             self.upload_nodestate(item, lastUpdate)
